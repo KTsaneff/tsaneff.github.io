c3c68d58c71374e6bf779fe13ce3414905e3db2a
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2024-10-28 23:50:22 +0200

    WorkingOver_BasicCommonFunctions

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,9 +1,14 @@
+import clsx from "clsx";
 import Image from "next/image";
+import {cn} from "@/lib/utils"
 
 export default function Home() {
   return (
     <div>
-      
+      <h1 className={cn("italic",
+        "after:",
+        "before:"
+      )}>Hello</h1>
     </div>
   );
 }
