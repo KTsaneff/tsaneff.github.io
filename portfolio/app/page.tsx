1d7ffe6173e2861913e4fe2edf27ef9bf3ab1eec
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2024-11-14 18:07:14 +0200

    Creating-the-Button-Component

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,6 +1,9 @@
 "use client";
+import Button from "@/components/ui/button";
 import Card from "@/components/ui/card";
 import WaterWaveWrapper from "@/components/visualEffects/water-wave-wrapper";
+import { GoHomeFill } from "react-icons/go";
+import { FaUser } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -12,7 +15,23 @@ export default function Home() {
     >
       {()=><div className="w-full p-10">
         <div className="max-w-2xl mx-auto">
-          <Card title="UI Components">...</Card>
+          <Card title="UI Components">
+            <div className="grid grid-cols-4">
+              <Button>
+                Basic button
+              </Button>
+              <Button>
+                <GoHomeFill/>
+                Basic button
+              </Button>
+              <Button isIcon>
+                <FaUser/>
+              </Button>
+              <Button link="https://www.google.com">
+                Google
+              </Button>
+            </div>
+          </Card>
         </div>        
       </div>}
   </WaterWaveWrapper>
