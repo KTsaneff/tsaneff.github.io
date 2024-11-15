60f02c76d86ce7ed04c1734ba0806707dd8231d8
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2024-11-15 11:37:09 +0200

    Creating-Fancy-Button-Component

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -3,10 +3,11 @@ import Button from "@/components/ui/button";
 import Card from "@/components/ui/card";
 import WaterWaveWrapper from "@/components/visualEffects/water-wave-wrapper";
 import { GoHomeFill } from "react-icons/go";
-import { FaUser } from "react-icons/fa";
+import { FaArrowRight, FaUser } from "react-icons/fa";
 import Input from "@/components/ui/input";
 import TextArea from "@/components/ui/text-area";
 import Profile from "@/components/ui/profile";
+import FancyButton from "@/components/ui/fancy-button";
 
 export default function Home() {
   return (
@@ -39,6 +40,8 @@ export default function Home() {
             <TextArea placeholder="Full name"/>
             <TextArea placeholder="Full name" icon={<FaUser/>}/>
             <Profile/>
+            <div className="w-[350px]">
+            <FancyButton text="Contact me" icon={<FaArrowRight/>}/></div>            
           </Card>
         </div>        
       </div>}
